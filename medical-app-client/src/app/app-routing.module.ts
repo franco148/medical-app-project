54181139e519952dcf899f8d74d4072a6cd385a7
franco-extreme <franck_ral@hotmail.com>
2018-08-01 12:07:59 -0400

    medical-app-frontend-stage04: Creating routing for new components that were created.

--- medical-app-client/src/app/app-routing.module.ts
+++ medical-app-client/src/app/app-routing.module.ts
@@ -2,14 +2,48 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PatientComponent } from './components/patient/patient.component';
 import { PatientEditionComponent } from './components/patient/patient-edition/patient-edition.component';
+import { ForbiddenPageComponent } from './components/forbidden-page/forbidden-page.component';
+import { ConsultationComponent } from './components/consultation/consultation.component';
+import { DoctorComponent } from './components/doctor/doctor.component';
+import { DoctorEditionComponent } from './components/doctor/doctor-edition/doctor-edition.component';
+import { ExamComponent } from './components/exam/exam.component';
+import { ExamEditionComponent } from './components/exam/exam-edition/exam-edition.component';
+import { SpecialtyComponent } from './components/specialty/specialty.component';
+import { SpecialtyEditionComponent } from './components/specialty/specialty-edition/specialty-edition.component';
 
 const routes: Routes = [
+  { path: 'forbidden403', component: ForbiddenPageComponent },
+  { path: 'consultation', component: ConsultationComponent },
   {
     path: 'patient',
     component: PatientComponent,
     children: [
       { path: 'new', component: PatientEditionComponent },
       { path: 'edition/:id', component: PatientEditionComponent }
+    ]
+  },
+  {
+    path: 'doctor',
+    component: DoctorComponent,
+    children: [
+      { path: 'new', component: DoctorEditionComponent },
+      { path: 'edition/:id', component: DoctorEditionComponent }
+    ]
+  },
+  {
+    path: 'exam',
+    component: ExamComponent,
+    children: [
+      { path: 'new', component: ExamEditionComponent },
+      { path: 'edition/:id', component: ExamEditionComponent }
+    ]
+  },
+  {
+    path: 'specialty',
+    component: SpecialtyComponent,
+    children: [
+      { path: 'new', component: SpecialtyEditionComponent },
+      { path: 'edition/:id', component: SpecialtyEditionComponent }
     ]
   }
 ];
