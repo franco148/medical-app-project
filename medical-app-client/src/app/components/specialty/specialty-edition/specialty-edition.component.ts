29fb600ea5ef8ee3529c2887f165c1df1fe349ea
franco-extreme <franck_ral@hotmail.com>
2018-08-02 18:17:01 -0400

    medical-app-frontend-stage5: Implemented components for Specialty Edition component.

--- medical-app-client/src/app/components/specialty/specialty-edition/specialty-edition.component.ts
+++ medical-app-client/src/app/components/specialty/specialty-edition/specialty-edition.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Specialty } from '../specialty.model';
+import { FormGroup, FormControl } from '../../../../../node_modules/@angular/forms';
+import { SpecialtyService } from '../../../services/specialty.service';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-specialty-edition',
@@ -7,9 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecialtyEditionComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  specialty: Specialty;
+  form: FormGroup;
+  isEdition = false;
 
-  ngOnInit() {
+  constructor(private specialtyService: SpecialtyService,
+              private route: ActivatedRoute,
+              private router: Router) {
+    this.specialty = new Specialty();
+    this.form = new FormGroup({
+      'id': new FormControl(0),
+      'name': new FormControl('')
+    });
   }
 
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      this.isEdition = params['id'] != null;
+      this.initForm();
+    });
+  }
+
+  private initForm() {
+    if (this.isEdition) {
+      this.specialtyService.findById(this.id).subscribe(result => {
+        const id = result.id;
+        const name = result.name;
+        this.form = new FormGroup({
+          'id': new FormControl(id),
+          'name': new FormControl(name)
+        });
+      });
+    }
+  }
+
+  operate() {
+    this.specialty.id = this.form.value['id'];
+    this.specialty.name = this.form.value['name'];
+
+    if (this.specialty != null && this.specialty.id > 0) {
+      this.specialtyService.edit(this.specialty).subscribe(data => {
+        this.specialtyService.findAll().subscribe(result => {
+          this.specialtyService.specialtyChanges.next(result);
+          this.specialtyService.message.next('Specialty has been updated successfully.');
+        });
+      });
+    } else {
+      this.specialtyService.register(this.specialty).subscribe(data => {
+        this.specialtyService.findAll().subscribe(result => {
+          this.specialtyService.specialtyChanges.next(result);
+          this.specialtyService.message.next('Specialty has been registered successfully.');
+        });
+      });
+    }
+
+    this.router.navigate(['specialty']);
+  }
 }
