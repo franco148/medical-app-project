6f1d9af999017eebaf18ab9d8bc4671c371342d7
franco-extreme <franck_ral@hotmail.com>
2018-08-02 18:52:56 -0400

    medical-app-frontend-stage5: Implemented components for Exam Components.

--- medical-app-client/src/app/components/exam/exam.component.ts
+++ medical-app-client/src/app/components/exam/exam.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort, MatSnackBar } from '@angular/material';
+import { Exam } from './exam.model';
+import { ExamService } from '../../services/exam.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-exam',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExamComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['id', 'name', 'description', 'actions'];
+  dataSource: MatTableDataSource<Exam>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  mensaje: string;
 
-  ngOnInit() {
+  constructor(private examService: ExamService,
+              public route: ActivatedRoute,
+              public snackBar: MatSnackBar) {
+
   }
 
+  ngOnInit() {
+    this.examService.examChanges.subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+
+    this.examService.findAll().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+
+    this.examService.message.subscribe(data => {
+      this.snackBar.open(data, null, {
+        duration: 2000,
+      });
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
+  remove(exam: Exam): void {
+    this.examService.remove(exam.id).subscribe(data => {
+      if (data === 1) {
+        this.examService.findAll().subscribe(result => {
+          this.examService.examChanges.next(result);
+          this.examService.message.next('Exam has been removed successfully.');
+        });
+      }
+    });
+  }
 }
